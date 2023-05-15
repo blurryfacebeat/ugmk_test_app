eead15cfc9ec064ef78e56133076d3ed03a53c6f
blurryfacebeat <blurryfacebeat@gmail.com>
2023-05-15 17:34:20 +0700

    feat: Update filter local storage

--- src/modules/Products/presentation/ProductsHome/helpers/useFilterLocalStorage.ts
+++ src/modules/Products/presentation/ProductsHome/helpers/useFilterLocalStorage.ts
@@ -1,11 +1,22 @@
-import { TProductFilterTypeKeyof } from '../../../types';
+import { PRODUCTS_FILTER_TYPE, TProductFilterTypeKeyof } from '../../../types';
 
 export const useFilterLocalStorage = () => {
   const setFilterValue = (value: TProductFilterTypeKeyof) =>
     localStorage.setItem('filterValue', value);
 
-  const getFilterValue = () =>
-    localStorage.getItem('filterValue') as TProductFilterTypeKeyof;
+  const getFilterValue = () => {
+    const result = localStorage.getItem(
+      'filterValue',
+    ) as TProductFilterTypeKeyof;
+
+    if (!Object.keys(PRODUCTS_FILTER_TYPE).includes(result)) {
+      setFilterValue(PRODUCTS_FILTER_TYPE.all);
+
+      return PRODUCTS_FILTER_TYPE.all;
+    }
+
+    return result;
+  };
 
   return {
     setFilterValue,
